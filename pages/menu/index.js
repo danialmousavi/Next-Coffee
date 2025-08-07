8efef91e627c59de8feb0c2082826e5ef05cb3b0
danialmousavi <danialmoosavi69@gmail.com>
2025-08-07 16:32:39 +0330

    create menu page

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -1,7 +1,23 @@
+import PageHeader from '@/components/modules/PageHeader/PageHeader'
+import Pricing from '@/components/templates/Menu/Pricing'
 import React from 'react'
 
-export default function Menu() {
+export default function Menu({data}) {
   return (
-    <div>index</div>
+    <>
+    <PageHeader route={"Menu"}/>
+    <Pricing menu={data}/>
+    </>
   )
+}
+export async function getStaticProps() {
+  const res=await fetch("http://localhost:4000/menu")
+  const data=await res.json();
+
+  return{
+    props:{
+      data
+    },
+    revalidate:60*60*6 //every 6 hour
+  }
 }
