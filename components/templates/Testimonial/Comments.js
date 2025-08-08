4672e05ca89802aa6613fe28ea0c8a73a5418e2f
danialmousavi <danialmoosavi69@gmail.com>
2025-08-08 12:49:46 +0330

    comments page

---
+++ components/templates/Testimonial/Comments.js
@@ -0,0 +1,22 @@
+import TestimonialItem from '@/components/modules/TestimonialItem/TestimonialItem';
+import React from 'react'
+
+export default function Comments({commentsData}) {
+    console.log("Commentsdata",commentsData);
+    
+  return (
+    <div className="container-fluid py-5">
+        <div className="container">
+            <div className="section-title">
+                <h4 className="text-primary text-uppercase" style={{letterSpacing: "5px"}}>Testimonial</h4>
+                <h1 className="display-4">Our Clients Say</h1>
+            </div>
+            <div className="owl-carousel testimonial-carousel">
+                {commentsData.map(item=>(
+                    <TestimonialItem key={item.id} {...item}/>
+                ))}                
+            </div>
+        </div>
+    </div>
+  )
+}
--- pages/testimonial/index.js
+++ pages/testimonial/index.js
@@ -1,7 +1,21 @@
+import PageHeader from '@/components/modules/PageHeader/PageHeader'
+import Comments from '@/components/templates/Testimonial/Comments';
 import React from 'react'
 
-export default function Testimonial() {
+export default function Testimonial({commentsData}) {
   return (
-    <div>index</div>
+    <>
+    <PageHeader route={"Comments"}/>
+    <Comments commentsData={commentsData}/>
+    </>
   )
+}
+export async function getStaticProps() {
+  const res=await fetch("http://localhost:4000/comments");
+  const commentsData=await res.json();
+  return{
+    props:{
+      commentsData
+    }
+  }
 }
